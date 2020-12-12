5b87441b57125eb0afc02863a9f4dbf5a14dded3
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2020-12-12 15:32:31 +0400

    add date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,16 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const fullYear = date.getFullYear();
+  if ((fullYear % 4) !== 0) {
+    return false;
+  } if ((fullYear % 100) !== 0) {
+    return true;
+  } if ((fullYear % 400) !== 0) {
+    return false;
+  }
+  return true;
 }
 
 
@@ -73,8 +81,13 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const different = new Date(endDate.getTime() - startDate.getTime());
+  const hours = String(different.getUTCHours()).padStart(2, '0');
+  const minutes = String(different.getUTCMinutes()).padStart(2, '0');
+  const seconds = String(different.getUTCSeconds()).padStart(2, '0');
+  const milliseconds = String(different.getUTCMilliseconds()).padStart(3, '0');
+  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
 
